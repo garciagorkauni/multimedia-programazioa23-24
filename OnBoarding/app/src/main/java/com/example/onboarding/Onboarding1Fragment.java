cef3aaad3762b70794d8f8f780d74b0bf3296ada
Gorka Garcia <garcia.gorka@uni.eus>
2023-09-27 08:35:04 +0200

    Fix error
    ERROR: java.lang.NullPointerException: Attempt to invoke virtual method 'void android.widget.Button.setOnClickListener(android.view.View$OnClickListener)' on a null object reference

--- OnBoarding/app/src/main/java/com/example/onboarding/Onboarding1Fragment.java
+++ OnBoarding/app/src/main/java/com/example/onboarding/Onboarding1Fragment.java
@@ -21,7 +21,7 @@ public class Onboarding1Fragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_onboarding2, container, false);
+        return inflater.inflate(R.layout.fragment_onboarding1, container, false);
     }
 
     @Override
