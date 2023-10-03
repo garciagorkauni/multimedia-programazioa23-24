b176db453c0b22ae61c56fb67c356c3b7145a5d4
Gorka Garcia <garcia.gorka@uni.eus>
2023-10-03 12:54:52 +0200

    Enter data (images missing)

--- Master-Detail/app/src/main/java/eus/urko/recyclerviewfragments/ElementsRepository.java
+++ Master-Detail/app/src/main/java/eus/urko/recyclerviewfragments/ElementsRepository.java
@@ -11,8 +11,18 @@ public class ElementsRepository {
         void whenFinish(List<Element> elements);
     }
     ElementsRepository(){
-        elements.add(new Element(R.drawable.principito, "El principito","Unreal Engine es un motor de juegos creado por la compañía Epic Games, dado a conocer inicialmente en el shooter en primera persona Unreal en 1998."));
-        elements.add(new Element(R.drawable.unrealengine_original, "Unreal Engine","Unreal Engine es un motor de juegos creado por la compañía Epic Games, dado a conocer inicialmente en el shooter en primera persona Unreal en 1998."));
+        elements.add(new Element(R.drawable.principito, "El principito","Un pequeño príncipe viaja de planeta en planeta y descubre lecciones sobre la vida y el amor en su búsqueda de amigos."));
+        elements.add(new Element(R.drawable.unrealengine_original, "Cien años de soledad","Una saga familiar que abarca siete generaciones en el pueblo ficticio de Macondo, explorando temas de amor, soledad, magia y decadencia."));
+        elements.add(new Element(R.drawable.unrealengine_original, "1984","En un mundo totalitario, Winston Smith lucha por la verdad y la libertad mientras es perseguido por el Partido y su líder, el omnipresente Gran Hermano."));
+        elements.add(new Element(R.drawable.unrealengine_original, "El nombre del viento","Kvothe, un talentoso músico y mago, narra su vida desde la infancia hasta su ascenso como leyenda, desvelando secretos y magia en un mundo de fantasía."));
+        elements.add(new Element(R.drawable.unrealengine_original, "Matar a un ruiseñor","La historia de Atticus Finch, un abogado que defiende a un hombre negro acusado de violar a una mujer blanca en la segregada Alabama de los años 30."));
+        elements.add(new Element(R.drawable.unrealengine_original, "Los juegos del hambre","Katniss Everdeen se convierte en símbolo de resistencia cuando participa en los Juegos del Hambre, una competición mortal impuesta por un gobierno opresivo."));
+        elements.add(new Element(R.drawable.unrealengine_original, "El código Da Vinci","Un profesor de simbología y una criptóloga investigan un misterioso asesinato que los lleva a descubrir un secreto antiguo que podría cambiar la historia del cristianismo."));
+        elements.add(new Element(R.drawable.unrealengine_original, "El gran Gatsby","La historia del enigmático Jay Gatsby y su obsesión por recuperar un amor del pasado en la decadente Nueva York de los años 20."));
+        elements.add(new Element(R.drawable.unrealengine_original, "Orgullo y prejuicio","La novela sigue la vida de Elizabeth Bennet mientras navega por las expectativas sociales y el romance en la Inglaterra del siglo XIX."));
+        elements.add(new Element(R.drawable.unrealengine_original, "Crónica de una muerte anunciada","La narrativa de la investigación periodística sobre el asesinato de Santiago Nasar en un pueblo caribeño, donde todos saben lo que va a ocurrir."));
+        elements.add(new Element(R.drawable.unrealengine_original, "La sombra del viento","En la Barcelona de posguerra, un joven encuentra un libro misterioso y comienza una búsqueda para descubrir la vida del autor y los secretos que rodean la obra."));
+        elements.add(new Element(R.drawable.unrealengine_original, "Los hombres me explican cosas","En este libro de ensayos, Rebecca Solnit aborda el sexismo y la condescendencia hacia las mujeres en la sociedad contemporánea, a través de anécdotas personales y análisis cultural."));
     }
     public List<Element> get() {
         return elements;
