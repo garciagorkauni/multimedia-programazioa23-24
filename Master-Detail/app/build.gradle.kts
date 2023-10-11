173a354a5702866f066bbe42ae4510d49af452c7
Gorka Garcia <garcia.gorka@uni.eus>
2023-10-11 09:06:20 +0200

    Update minSDK Master-Detail

--- Master-Detail/app/build.gradle.kts
+++ Master-Detail/app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
 
     defaultConfig {
         applicationId = "eus.urko.recyclerviewfragments"
-        minSdk = 29
+        minSdk = 27
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
