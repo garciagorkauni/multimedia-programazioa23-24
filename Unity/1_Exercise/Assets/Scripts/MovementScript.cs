e8f26a69a56d3ae0b21eeb35ac913e04a95ef06d
Gorka Garcia <garcia.gorka@uni.eus>
2023-12-04 13:12:26 +0100

    Fixed first exercise

--- Unity/1_Exercise/Assets/Scripts/MovementScript.cs
+++ Unity/1_Exercise/Assets/Scripts/MovementScript.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq.Expressions;
@@ -9,6 +10,9 @@ public class MovementScript : MonoBehaviour
     public float speed = 5f;
     public string direction;
     public Vector3 movement;
+    public int gradeChange = 90;
+    public int actualGrade = 0;
+    public Boolean needsRotation = false;
 
     // Start is called before the first frame update
     void Start()
@@ -22,30 +26,46 @@ public class MovementScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Move the object
-        transform.position += movement*speed*Time.deltaTime;
+        // Rotate the object
+        if (needsRotation)
+        {
+            actualGrade++;
+            transform.Rotate(0, 1, 0);
+            if (actualGrade == gradeChange)
+            {
+                actualGrade = 0;
+                needsRotation = false;
+            }
+        } else
+        {
+            // Move the object
+            transform.position += movement * speed * Time.deltaTime;
 
-        // Define the movement depending on each direction
-        if (direction.Equals("Forward") && transform.position.z >= 10)
-        {
-            direction = "Right";
-            movement = Vector3.right;
-        } 
-        else if (direction.Equals("Right") && transform.position.x >= 10)
-        {
-            direction = "Back";
-            movement = Vector3.back;
+            // Define the movement depending on each direction
+            if (direction.Equals("Forward") && transform.position.z >= 10)
+            {
+                needsRotation = true;
+                direction = "Right";
+                movement = Vector3.right;
+            }
+            else if (direction.Equals("Right") && transform.position.x >= 10)
+            {
+                needsRotation = true;
+                direction = "Back";
+                movement = Vector3.back;
+            }
+            else if (direction.Equals("Back") && transform.position.z <= 0)
+            {
+                needsRotation = true;
+                direction = "Left";
+                movement = Vector3.left;
+            }
+            else if (direction.Equals("Left") && transform.position.x <= 0)
+            {
+                needsRotation = true;
+                direction = "Forward";
+                movement = Vector3.forward;
+            }
         }
-        else if (direction.Equals("Back") && transform.position.z <= 0)
-        {
-            direction = "Left";
-            movement = Vector3.left;
-        }
-        else if (direction.Equals("Left") && transform.position.x <= 0)
-        {
-            direction = "Forward";
-            movement = Vector3.forward;
-        }
-
     }
 }
